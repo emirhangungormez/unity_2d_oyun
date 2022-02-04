fd62d5da3c7807d0bf880870f73dcae951d25f89
emir <89928334+emirhangungormez@users.noreply.github.com>
2022-02-04 05:13:13 +0300

    Update beniyoket.cs

--- assets/Scripts/beniyoket.cs
+++ assets/Scripts/beniyoket.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class beniyoket : MonoBehaviour
 {
     public int lifeTime;
-    // Start is called before the first frame update
+    
     void Start()
     {
         Destroy(gameObject, lifeTime);
-        // belli sbir süre zarfından sonra kodun üzerinde bulunduğu game objesi kendisini yok edecektir.
-        
+        // belli sbir süre zarfından sonra kodun üzerinde bulunduğu game objesi kendisini yok edecektir.    
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
